c2bbd153b6a262ee3c88bb79f4f771425e758ffb
tiboCor <joebarteam5962@gmail.com>
2016-12-09 11:57:53 +0100

    creation hydrateur

---
+++ Hydrateur.java
@@ -0,0 +1,14 @@
+package marcytial;
+
+public class Hydrateur
+{
+	private Visuel visuel;
+	private Reader reader;
+	
+   public Hydrateur()
+   {
+	  visuel= new Visuel();
+	  // reader= new Reader();
+   }	
+	
+}
