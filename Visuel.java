c505529affbf543d8d24fe5fc0e4b71fa6af5bdb
alexis rogez <alexis.noexis@gmail.com>
2016-12-09 11:54:17 +0100

    creation Visuel.java

---
+++ Visuel.java
@@ -0,0 +1,14 @@
+package marcytial;
+import javax.swing.JFrame;
+/**
+ *
+ * @author rogeza
+ */
+public class Visuel {
+
+  public static void main(String[] args){
+    JFrame fenetre = new JFrame();
+    fenetre.setVisible(true);
+  }       
+}
+
