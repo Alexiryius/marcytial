8de3fd225a69a2913f2ad9e483c6fb6d5a294357
Marcelin eboma <marcelebola@gmail.com>
2017-01-04 16:39:13 +0100

    MAJ

--- SuiteChrono.java
+++ SuiteChrono.java
@@ -1,19 +1,14 @@
 package marcytial;
 
 public class SuiteChrono {
-	
 	private String date;
-	private double open, high, low, close;
+	private double open, high;
 
-	public SuiteChrono() {
-		
-	}
-		public SuiteChrono(String date, double open) {
+	public SuiteChrono(String date, double open) {
 		this.date = date;
 		this.open = open;
 		
-	}
-
+		}
 	public String getDate() {
 		return date;
 	}
@@ -30,43 +25,15 @@ public class SuiteChrono {
 		this.open = open;
 	}
 
-
-
 	public double getHigh() {
 		return high;
 	}
 
-	public void setHigh(double high) {
-		this.high = high;
-	}
-
-	public double getLow() {
-		return low;
-	}
-
-	public void setLow(double low) {
-		this.low = low;
-	}
-
-
-
-	public double getClose() {
-		return close;
-	}
-
-
-
-	public void setClose(double close) {
-		this.close = close;
-	}
-
 	public String toString()
     {
-        return getDate() +" | " + getOpen() ;
+        return getDate() +" | " + getOpen();
     }
 
 	
 
 }
-
-
