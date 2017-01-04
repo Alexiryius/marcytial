6eff6b639e227509d98d94253a8a4c1b3dff7051
alexis rogez <alexis.noexis@gmail.com>
2017-01-04 17:06:28 +0100

    un combobox et dispo de visuel

--- Visuel.java
+++ Visuel.java
@@ -1,6 +1,13 @@
 package marcytial;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JPanel;
 import org.jfree.chart.*; 
 import java.util.Random;
@@ -15,8 +22,58 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 
 public class Visuel extends JFrame{
-
-    public Visuel(){
+	
+	
+	
+	public JComboBox combo(){
+	String[] transfoTitles = new String[] {"transformation 1", "transformation 2", "transformation 3", "transformation 4"};	
+	//fenetre combobox choix des transformations
+	JComboBox<String> transfoList = new JComboBox<>(transfoTitles);
+	return transfoList;
+	}
+	
+	
+	
+	XYDataset createDataset() {
+		Random r = new Random();
+	    XYSeriesCollection result = new XYSeriesCollection();
+	    XYSeries series = new XYSeries("Random");
+	    for (int i = 0; i <= 100; i++) {
+	        double x = r.nextDouble();
+	        double y = r.nextDouble();
+	        series.add(x, y);
+	    }
+	    result.addSeries(series);
+	    return result;
+	}
+	
+	
+	public JPanel sPlotPan(){
+	JPanel sPlotPan = new JPanel();
+	//fenetre scatterplot
+	
+	JFreeChart chart = ChartFactory.createScatterPlot(
+	            "Scatter Plot", // chart title
+	            "X", // x axis label
+	            "Y", // y axis label
+	            createDataset(), // data  
+	            PlotOrientation.VERTICAL,
+	            true, // include legend
+	            true, // tooltips
+	            false // urls
+	            );
+	ChartPanel cPanel = new ChartPanel(chart);
+	
+	
+	sPlotPan.add(cPanel);
+	return sPlotPan;
+	
+	}
+	
+		
+	public Visuel(){
+    
+    	
     	Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
     	int hauteur = (int)dimension.getHeight();
     	int largeur  = (int)dimension.getWidth();
@@ -26,44 +83,51 @@ public class Visuel extends JFrame{
     	this.setLocationRelativeTo(null);
     	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     	
-    	this.setContentPane(new Graphique());
+    	
+    	 JPanel content = new JPanel();
+    	    content.setPreferredSize(new Dimension(300, 120));
+    	    //On définit le layout manager
+    	    content.setLayout(new GridBagLayout());
+    	    //L'objet servant à positionner les composants
+    	    GridBagConstraints gbc = new GridBagConstraints();
+ 
+    	//https://openclassrooms.com/courses/apprenez-a-programmer-en-java/positionner-des-boutons
+        //---------------------------------------------
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 2;
+        //Celle-ci indique que la cellule se réplique de façon verticale
+        gbc.fill = GridBagConstraints.VERTICAL;
+        content.add(cell5, gbc);
+        //---------------------------------------------
+        gbc.gridx = 1;
+        gbc.gridheight = 1;
+        //Celle-ci indique que la cellule se réplique de façon horizontale
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        content.add(cell6, gbc);
+        //---------------------------------------------
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        content.add(cell7, gbc);
+        //---------------------------------------------
+        gbc.gridx = 3;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        content.add(cell8, gbc);
+        //---------------------------------------------
+        this.setContentPane(content);
+        this.setVisible(true);      
+
+            
+    	
     	System.out.println("le constructeur est executé");
     } 
-    
-    
-    // classe panel ou est represente le graphique
-    public class Graphique extends JPanel { 
-    	
-    	private final Random r = new Random();
 
-    	private XYDataset createDataset() {
-    	    XYSeriesCollection result = new XYSeriesCollection();
-    	    XYSeries series = new XYSeries("Random");
-    	    for (int i = 0; i <= 100; i++) {
-    	        double x = r.nextDouble();
-    	        double y = r.nextDouble();
-    	        series.add(x, y);
-    	    }
-    	    result.addSeries(series);
-    	    return result;
-    	}
-    	
-    	public Graphique(){
-    		JFreeChart chart = ChartFactory.createScatterPlot(
-    	            "Scatter Plot", // chart title
-    	            "X", // x axis label
-    	            "Y", // y axis label
-    	            createDataset(), // data  ***-----PROBLEM------***
-    	            PlotOrientation.VERTICAL,
-    	            true, // include legend
-    	            true, // tooltips
-    	            false // urls
-    	            );
-    		ChartPanel cPanel = new ChartPanel(chart); 
-    		this.add(cPanel); 
-    		
-    	}
-    }
+
+
+    
     
     
     
