db4b92d6399757c0dafa879c8b3b66fae760d974
alexis rogez <alexis.noexis@gmail.com>
2017-01-12 13:39:06 +0100

    création pour les actionlistener : ActionEcoute.java

---
+++ ActionEcoute.java
@@ -0,0 +1,31 @@
+package marcytial;
+import marcytial.Visuel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActionEcoute{
+
+static ActionListener monActionEcouteur = new ActionListener() {
+	boolean clic = false;	
+	
+	public void actionPerformed(ActionEvent e){
+		Object source=e.getSource();    
+	    if (source==Visuel.tab){
+	    	if(clic == false){
+	    		Visuel.tab.setText("graphique");
+	    		clic = true;
+	    	}
+	    	else{
+	    		Visuel.tab.setText("tableau");
+	    		clic = false;
+	    	}
+	    }
+		if (source==Visuel.validBouton){
+			Visuel.validBouton.setText("OK");
+		}
+	}
+}
+	;
+}
+
+
