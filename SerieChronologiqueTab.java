3826182ac4576dd47c09969e246f4f2f668ad31e
alexis rogez <alexis.noexis@gmail.com>
2017-01-12 09:13:26 +0100

    modif avec Place dans le code moche de tibo

--- SerieChronologiqueTab.java
+++ SerieChronologiqueTab.java
@@ -1,10 +1,12 @@
 package marcytial;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Vector;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -19,17 +21,29 @@ public class SerieChronologiqueTab extends Serie implements AffTab  {
 		super(nom,date,valeur);
 	}
 
+	private boolean isInitialized() {
+		return (this.getDate() != null) ;
+	}
+	
 	@Override
 	public JPanel returnPanel() {
-		
-		JTable tableau = new JTable(new Vector<Double>(this.getValeur()),new Vector<Date>(this.getDate()));
-		JScrollPane scrollPane = new JScrollPane(tableau);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
-        scrollPane.setBounds(50, 30, 300, 50);
-        JPanel contentPane = new JPanel(null);
-        contentPane.setPreferredSize(new Dimension(500, 400));
-        contentPane.add(scrollPane);
+        JPanel contentPane = new JPanel(null);	
+		if (isInitialized()) {
+			Vector<Date> listDate = new Vector<>(this.getDate());
+			Vector<Double> listValeur = new Vector<>(this.getValeur());
+			JTable tableau = new JTable(listDate, listValeur);
+			JScrollPane scrollPane = new JScrollPane(tableau);
+	        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+	        scrollPane.setBounds(50, 30, 300, 50);
+	        contentPane.add(scrollPane);
+		} else {
+			JLabel text = new JLabel("Y a rien") ;
+			contentPane.add(text) ;
+		}
+		contentPane.setSize(new Dimension(50, 50));
+        contentPane.setPreferredSize(new Dimension(50, 50));
+        contentPane.setBackground(Color.BLUE);
         
         return contentPane;
 	}
