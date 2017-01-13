f2a26f8f12d4d4499b4209b01feaac0ee6a441b5
tiboCor <joebarteam5962@gmail.com>
2017-01-13 08:52:00 +0100

    mange sa ma pute

--- Programme.java
+++ Programme.java
@@ -9,6 +9,9 @@ public class Programme{
 	public static void main(String[] args) throws IOException, ParseException {
 		
 		Hydrateur hydra  = new Hydrateur();
+		System.out.println(hydra.getLaSerie());
+		System.out.println(hydra.getLaSerie().getNom());
+		System.out.println(hydra.getLaSerie().getDate());
  	 
 	}
 } 
