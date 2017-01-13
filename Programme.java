3886deff5153efc6a87a6bd904ac0bbaff4892c2
alexis rogez <alexis.noexis@gmail.com>
2017-01-13 09:55:24 +0100

    push de début

--- Programme.java
+++ Programme.java
@@ -9,12 +9,9 @@ public class Programme{
 	public static void main(String[] args) throws IOException, ParseException {
 		
 		Hydrateur hydra  = new Hydrateur();
-<<<<<<< HEAD
-=======
 		System.out.println(hydra.getLaSerie());
 		System.out.println(hydra.getLaSerie().getNom());
 		System.out.println(hydra.getLaSerie().getDate());
  	 
->>>>>>> f2a26f8f12d4d4499b4209b01feaac0ee6a441b5
 	}
 } 
