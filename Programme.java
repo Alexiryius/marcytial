1528a18a59a54021239998b369e37663658064c4
alexis rogez <alexis.noexis@gmail.com>
2017-01-18 17:55:12 +0100

    moyenne mobile ok

--- Programme.java
+++ Programme.java
@@ -2,6 +2,7 @@ package marcytial;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.text.ParseException;
 
 import javax.swing.JFileChooser;
@@ -13,7 +14,10 @@ public class Programme{
 		
  new Hydrateur();
 
- 	
+ String pattern = "######.##";
+ DecimalFormat decimalFormat = new DecimalFormat(pattern);
+ String format = decimalFormat.format(456789.123);
+ System.out.println(Double.parseDouble(format));	
  //new Tools();
  //System.out.println(Serie.getCurrent().getValeur());
  //System.out.println(Serie.getCurrent().getDate());
