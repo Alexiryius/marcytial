71aefcfefdef6237a6a210c0de1886b095000b20
tiboCor <joebarteam5962@gmail.com>
2017-01-19 13:24:17 +0100

    mange sa petite pute en chocolat

--- Serie.java
+++ Serie.java
@@ -8,45 +8,32 @@ public abstract class Serie
 {   
 	
 	private static Serie current = null ;
-	private  static   int id=0 ;
-	private final int num;
 	private String nom = "";
 	private ArrayList<Date> date ;
 	private ArrayList<Double>valeur ;
-	private Transformation transformation;	
+	
 	
 	//Constructeur
 	Serie()
 	{
-		id ++;
-		num=id;
+		
 		nom ="no_name";
 		date=null;
 		valeur=null;
-		transformation=null;
+		
 		current=this;
 		
 	}
 	Serie(String nom, ArrayList<Date> date, ArrayList<Double> valeur)
 	{
-		id++;
-		this.num=id;
+		
 		this.nom = nom;
 		this.date = date;
 		this.valeur = valeur;
-		this.transformation = null;
+	
 		current=this;
 	}
-	Serie(String nom, ArrayList<Date> date, ArrayList<Double> valeur, Transformation transformation)
-	{
-		id++;
-		this.num=id;
-		this.nom = nom;
-		this.date = date;
-		this.valeur = valeur;
-		this.transformation = transformation;
-		current=this;
-	}
+	
 	
 	//private boolean tableau;
 	//private boolean grahique;
@@ -59,10 +46,7 @@ public abstract class Serie
 		return this.nom;
 	}
 	
-	@Override
-	public String toString() {
-		return "["+this.num+ "]";
-	}
+	
 	public void setNom(String n)
 	{
 		this.nom = n;
@@ -83,10 +67,7 @@ public abstract class Serie
 		this.valeur = n;
 	}
 	
-	public Transformation getTransformation()
-	{
-		return this.transformation;
-	}
+	
 	
 	public void setDate(ArrayList<Date> bim)
 	{
